1d76465dc1f5379cef4e0f87ea1ee8b716e2cf64
Chris Crum <clcrum@gmail.com>
2022-05-07 13:01:51 -0500

    refactor(setstateconfig): change setState to setStateConfig and ensure prior types don't change

--- src/lib/createPretext.ts
+++ src/lib/createPretext.ts
@@ -2,23 +2,23 @@ import { isFunction } from '../helpers/isFunction';
 
 /**
  * The internals of the pretext engine.
- * Why is this a class? There are certain typing patterns that are easier to achieve this way.
+ * Why is this a class? There are certain typing patterns that are easier and cleaner to achieve this way.
  */
-class Pretext<CpPretextState extends object> {
+export class Pretext<CpPretextConfigState extends object> {
   /**
-   * The internal state.
+   * The internal configuration state.
    * Each prop within this state object will be wrapped in a render-friendly and reactive atom.
    */
-  state: CpPretextState = {} as CpPretextState;
+  configState: CpPretextConfigState = {} as CpPretextConfigState;
 
   /**
-   * Sets the internal state.
+   * Sets the internal state configuration.
    * Note: this does not
-   * @param state An object of properties representing the internal pretext state.
+   * @param state A configuration object of properties representing the internal pretext state default.
    * @return The pretext shell so it can be chainable.
    */
-  setState<State extends object>(state: State | (() => State)) {
-    this.state = (isFunction(state) ? state() : state) as any as CpPretextState;
+  setStateConfig<State extends CpPretextConfigState>(state: State | (() => State)) {
+    this.configState = (isFunction(state) ? state() : state) as any as CpPretextConfigState;
     return this as any as Pretext<State>;
   }
 }
