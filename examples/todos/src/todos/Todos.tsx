54348983a780cbd19e2b0844c8e6eb340c393d3c
Crum <clcrum@gmail.com>
2022-05-14 08:40:13 -0500

    docs(todos): add pretext-state dependency and render out internal state

--- examples/todos/src/todos/Todos.tsx
+++ examples/todos/src/todos/Todos.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { createPretext } from 'pretext-state';
+
+const todosPretext = createPretext('todos', {
+  firstName: 'jimmy',
+});
 
 export function Todos() {
-  return <div>Todos</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      <div>{todosPretext._configState.firstName}</div>
+    </div>
+  );
 }
